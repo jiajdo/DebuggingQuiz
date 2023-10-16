60d94026349bed3d69a86322a6de982119922486
Jia <jovsofjdo@gmail.com>
2023-10-16 10:24:30 -0700

    Debugged frontend server and App.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4040;
 
 app.listen(PORT, () => console.log("Server started"));
 
@@ -19,6 +19,9 @@ const items = [
   },
 ];
 
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
 app.get("/api/items", (req, res) => {
   res.send(items);
 });
